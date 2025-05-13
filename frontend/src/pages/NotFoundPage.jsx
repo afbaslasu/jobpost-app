0013a4e86418b0a046b6f3ac9fad56c537579f22
afbaslasu <afbaslasu@gmail.com>
2025-05-13 05:00:33 +0200

    chore: custom Not Found page created and added to the router-dom

---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaExclamationTriangle } from "react-icons/fa";
+
+const NotFoundPage = () => {
+  return (
+    <section className="text-center flex flex-col justify-center items-center min-h-[calc(100vh-5rem)]">
+      <FaExclamationTriangle className="text-yellow-400 text-6xl" />
+      <h1 className="text-6xl font-bold p-4">404 Not Found</h1>
+      <p className="text-xl mb-5">This page does not exist</p>
+      <Link
+        to="/"
+        className="text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4"
+      >
+        Go Back
+      </Link>
+    </section>
+  );
+};
+
+export default NotFoundPage;
