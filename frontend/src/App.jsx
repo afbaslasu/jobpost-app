c0ce353fb452f6de977b1c90a8f9bf32364403ad
afbaslasu <afbaslasu@gmail.com>
2025-05-13 03:29:55 +0200

    chore: react-router-dom installed, and page layout setup, and homepage created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,19 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element ={ <HomePage /> } />)
+)
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Hero
-      // title="Tech Job Post Main Title"
-      // subtitle="Tech Job Post Subtitle should be written here"
-      />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-
-    </>
+    <RouterProvider router={router} />
   );
 };
 
