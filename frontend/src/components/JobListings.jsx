948297fd3525e0f541844ef41b4f534242127dec
afbaslasu <afbaslasu@gmail.com>
2025-05-24 03:00:46 +0200

    chore: express backend setup

--- frontend/src/components/JobListings.jsx
+++ frontend/src/components/JobListings.jsx
@@ -2,28 +2,33 @@ import React from "react";
 import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
 import Spinners from "./Spinners";
+import jobsData from "../jobs.json";
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchJobs = async () => {
-      const apiUrl = isHome
-        ? "/api/jobs?_limit=3"
-        : "/api/jobs";
-      try {
-        const res = await fetch(apiUrl);
-        const data = await res.json();
-        setJobs(data);
-      } catch (error) {
-        console.log("Error fetching data", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchJobs = async () => {
+  //     const apiUrl = isHome
+  //       ? "/api/jobs?_limit=3"
+  //       : "/api/jobs";
+  //     try {
+  //       const res = await fetch(apiUrl);
+  //       const data = await res.json();
+  //       setJobs(data);
+  //     } catch (error) {
+  //       console.log("Error fetching data", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    fetchJobs();
+  //   fetchJobs();
+  // }, []);
+  useEffect(() => {
+    setJobs(isHome ? jobsData.jobs.slice(0, 3) : jobsData.jobs);
+    setLoading(false);
   }, []);
 
   return (
